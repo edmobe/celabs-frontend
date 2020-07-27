26a88c3f6a24d71ec264ac0ee1806ec5b5daf568
edmobe <31488944+edmobe@users.noreply.github.com>
2020-07-26 21:48:08 -0600

    Update login, navbar and Angular project structure

---
+++ src/app/_components/lab-reservation/lab-reservation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lab-reservation',
+  templateUrl: './lab-reservation.component.html',
+  styleUrls: ['./lab-reservation.component.css']
+})
+export class LabReservationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './_components/login/login.component';
+import { LabReservationComponent } from './_components/lab-reservation/lab-reservation.component';
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    LabReservationComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
