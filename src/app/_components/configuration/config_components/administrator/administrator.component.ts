045ee9438ae58da12cdd3235453027ccfb494635
Bamdres16 <brianm.bra@estudiantec.cr>
2020-08-08 20:59:14 -0600

    administrator table updated

--- src/app/_components/configuration/config_components/administrator/administrator.component.ts
+++ src/app/_components/configuration/config_components/administrator/administrator.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleService } from 'src/app/_services/title.service';
+
+interface User {
+  nombre: string;
+  correo: string;
+  rol: string;
+  permisos: boolean;
+}
+
 @Component({
   selector: 'app-administrator',
   templateUrl: './administrator.component.html',
