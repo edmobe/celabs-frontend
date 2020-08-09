d279e981cd2ec22f53890629c29d42c4a6cb2cd2
Bamdres16 <brianm.bra@estudiantec.cr>
2020-08-09 17:29:34 -0600

    Admin view

--- src/app/_components/configuration/config_components/administrator/administrator.component.ts
+++ src/app/_components/configuration/config_components/administrator/administrator.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TitleService } from 'src/app/_services/title.service';
 
 interface User {
+  id: number;
   nombre: string;
   correo: string;
   rol: string;
@@ -16,13 +17,23 @@ interface User {
 export class AdministratorComponent implements OnInit {
 
   users : User[] = [
-    {nombre: 'Luis Diego Noguera',correo: 'lnoguera@itcr.ac.cr', rol: 'admininistrador', permisos: false}
+    {id: 0, nombre: 'Luis Diego Noguera',correo: 'lnoguera@itcr.ac.cr', rol: 'admininistrador', permisos: false},
+    {id: 1, nombre: 'Brayan Muñoz Mora',correo: 'brianm.bra@estudiantec.cr', rol: 'admininistrador', permisos: false}
   ];
   constructor(private titleService: TitleService) {
     this.titleService.setTitle('');
    }
   ngOnInit(): void {
     
+  }
+
+  checkValue (id: number, event: any) : void {
+    var permiso : boolean = event.currentTarget.checked;
+    if (permiso) {
+      document.getElementById("labelCheck"+id).textContent = "Permitido";
+    } else {
+      document.getElementById("labelCheck"+id).textContent = "Denegado";
+    }
   }
 
 }
