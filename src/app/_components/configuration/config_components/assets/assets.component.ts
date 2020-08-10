83cc2b14a61766613d4448f3fee59eafa828a719
Bamdres16 <brianm.bra@estudiantec.cr>
2020-08-09 20:35:19 -0600

    admin view

--- src/app/_components/configuration/config_components/assets/assets.component.ts
+++ src/app/_components/configuration/config_components/assets/assets.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleService } from 'src/app/_services/title.service';
 
+interface Activos {
+  codigo : string;
+  descripcion : string;
+}
 @Component({
   selector: 'app-assets',
   templateUrl: './assets.component.html',
@@ -12,7 +16,16 @@ export class AssetsComponent implements OnInit {
     this.titleService.setTitle('');
    }
 
+  activos : Activos[] = [
+    {codigo: "CE1001", descripcion : "Monitor"},
+    {codigo: "CE1002", descripcion : "Monitor"}
+  ];
+
   ngOnInit(): void {
+  }
+
+  deleteAsset(activo : string) : void {
+    console.log(activo);
   }
 
 }
--- src/app/_components/configuration/config_components/csupport-team/csupport-team.component.ts
+++ src/app/_components/configuration/config_components/csupport-team/csupport-team.component.ts
@@ -1,6 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleService } from 'src/app/_services/title.service';
 
+interface User {
+  id: number;
+  nombre: string;
+  correo: string;
+  rol: string;
+  permisos: boolean;
+}
+
+
+var buttonDanger: string = "btn btn-danger";
+var buttonSuccess: string = "btn btn-success";
+
 @Component({
   selector: 'app-csupport-team',
   templateUrl: './csupport-team.component.html',
@@ -8,11 +20,27 @@ import { TitleService } from 'src/app/_services/title.service';
 })
 export class CsupportTeamComponent implements OnInit {
 
+  users : User[] = [
+    {id: 0, nombre: 'Luis Diego Noguera',correo: 'lnoguera@itcr.ac.cr', rol: 'admininistrador', permisos: false},
+    {id: 1, nombre: 'Brayan Muñoz Mora',correo: 'brianm.bra@estudiantec.cr', rol: 'admininistrador', permisos: true}
+  ];
   constructor(private titleService: TitleService) {
     this.titleService.setTitle('');
    }
-
   ngOnInit(): void {
+  }
+
+  checkValue (id: number) : void {
+    var button = (<HTMLInputElement>document.getElementById("btn"+id));
+    var clase = button.className;
+    if (clase == buttonSuccess) {
+      document.getElementById("btn"+id).className = buttonDanger;
+      button.value = "Denegado";
+    } else {
+      document.getElementById("btn"+id).className = buttonSuccess;
+      button.value = "Permitido";
+      
+    }
   }
 
 }
