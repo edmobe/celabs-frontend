48435e449737368bbf125ca3aed429195d2c8bfb
edmobe <31488944+edmobe@users.noreply.github.com>
2020-08-11 15:44:19 -0600

    Update dashboard

--- src/app/_components/dashboard/dashboard.component.ts
+++ src/app/_components/dashboard/dashboard.component.ts
@@ -10,44 +10,12 @@ import { faAngry, faFrown, faSmile, faLaugh } from '@fortawesome/free-regular-sv
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  // Information to get
   public occupation: number;
   public satisfaction: number;
-  public reservations = [{
-    semana: 1,
-    reservaciones: 4,
-    palmadas: 0
-  },
-  {
-    semana: 2,
-    reservaciones: 7,
-    palmadas: 1
-  },
-  {
-    semana: 3,
-    reservaciones: 2,
-    palmadas: 0
-  },
-  {
-    semana: 4,
-    reservaciones: 10,
-    palmadas: 2
-  },
-  {
-    semana: 5,
-    reservaciones: 3,
-    palmadas: 2
-  },
-  {
-    semana: 6,
-    reservaciones: 2,
-    palmadas: 3
-  },
-  {
-    semana: 7,
-    reservaciones: 6,
-    palmadas: 1
-  }];
+  public reservations: any;
 
+  // Charts
   public occupationChart: Chart;
   public reservationsChart: Chart;
 
@@ -62,10 +30,61 @@ export class DashboardComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.occupation = 30;
-    this.satisfaction = 80;
+    // GET requests
+    this.occupation = this.getOcupation();
+    this.satisfaction = this.getSatisfaction();
+    this.reservations = this.getReservations();
+
+    // Chart generation
     this.occupationChart = this.chartGeneratorService.getOccupationChart(this.occupation);
     this.reservationsChart = this.chartGeneratorService.getReservationsChart('reservations', this.reservations);
   }
 
+  /* GET requests */
+  getOcupation(): number {
+    return 30;
+  }
+
+  getSatisfaction(): number {
+    return 80;
+  }
+
+  getReservations(): any {
+    const reservations = [{
+      semana: 1,
+      reservaciones: 4,
+      palmadas: 0
+    },
+    {
+      semana: 2,
+      reservaciones: 7,
+      palmadas: 1
+    },
+    {
+      semana: 3,
+      reservaciones: 2,
+      palmadas: 0
+    },
+    {
+      semana: 4,
+      reservaciones: 10,
+      palmadas: 2
+    },
+    {
+      semana: 5,
+      reservaciones: 3,
+      palmadas: 2
+    },
+    {
+      semana: 6,
+      reservaciones: 2,
+      palmadas: 3
+    },
+    {
+      semana: 7,
+      reservaciones: 6,
+      palmadas: 1
+    }];
+    return reservations;
+  }
 }
