c0aa7bed2a829f0850b47a09962e5aa7c9234244
Gerald Valverde <gerald-_16@hotmail.com>
2020-08-14 03:44:00 -0600

    Fix imports

--- src/app/_services/api/account.service.ts
+++ src/app/_services/api/account.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Admin } from '../../_models/admin.model';
+import { ApiService } from '../api/api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
