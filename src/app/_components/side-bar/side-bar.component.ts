5b9b9e9b08bfc3f93ce09ce92647e9cf3f74c50d
Bamdres16 <brianm.bra@estudiantec.cr>
2020-08-13 19:30:34 -0600

    sidebar modifications for new views

--- src/app/_components/side-bar/side-bar.component.ts
+++ src/app/_components/side-bar/side-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
@@ -7,8 +7,10 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
+  public Clip = faCheckCircle;
+  
   constructor() { }
-
+  
   ngOnInit(): void { }
 
 }
